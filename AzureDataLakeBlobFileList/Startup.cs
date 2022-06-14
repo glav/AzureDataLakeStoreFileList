4ed20d71df60ac782bb20d90ca87e3dca7fa106a
Paul Glavich <glav@theglavs.com>
2022-06-14 14:58:52 +1000

    Added in simple config file support

---
+++ AzureDataLakeBlobFileList/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureDataLakeBlobFileList
+{
+    internal class Startup
+    {
+        public void Configure()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.json");
+
+            var config = configuration.Build();
+            var connectionString = config["ConnectionString"];
+        }
+    }
+}
